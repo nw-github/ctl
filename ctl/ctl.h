5a4fba991377d247f168ca88cf8edeb92c88a192
Nathaniel W <52637288+nw-github@users.noreply.github.com>
2023-07-27 18:25:38 -0400

    include stddef.h (for NULL)

--- ctl/ctl.h
+++ ctl/ctl.h
@@ -1,16 +1,16 @@
 #include <gc.h>
 #include <stdint.h>
+#include <stddef.h>
 
 #define CTL_SBITINT(bits) _BitInt(bits)
 #define CTL_UBITINT(bits) unsigned _BitInt(bits)
 
-typedef intptr_t    isize;
-typedef uintptr_t   usize;
-typedef uint32_t    CTL_char;
-typedef uint8_t     CTL_bool;
-
-typedef float       f32;
-typedef double      f64;
+typedef intptr_t  isize;
+typedef uintptr_t usize;
+typedef uint32_t  CTL_char;
+typedef uint8_t   CTL_bool;
+typedef float     f32;
+typedef double    f64;
 
 static inline void *ctl_malloc(usize size) {
     return GC_MALLOC(size);
